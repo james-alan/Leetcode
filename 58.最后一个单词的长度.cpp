b0b31982cf08aac924027057dbe680c992790310
糖吃多了牙会疼 <xch13779509255@gmail.com>
2024-08-22 09:14:49 +0800

    add files

--- 58.最后一个单词的长度.cpp
+++ 58.最后一个单词的长度.cpp
@@ -8,6 +8,18 @@
 class Solution {
 public:
     int lengthOfLastWord(string s) {
+        int count=0;
+
+        for(int i=s.size()-1;i>=0;i--){
+            if(s[i]==' '&&count == 0)
+                continue;
+            if(s[i]==' '&&count!= 0) 
+                break;
+            if(s[i]!=' ')
+                count++;
+
+        }
+        return count;
 
     }
 };
